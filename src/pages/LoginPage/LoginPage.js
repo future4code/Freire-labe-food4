6f531ee23d371e5b2f904e42a2f9071f21ea5247
Murilo Cardoso <murilo_mac@hotmail.com>
2022-08-01 17:41:40 -0300

    page login em andamento, inputs e botao ok

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,28 @@
 import React from "react";
+import LogoRappi from '../../imgs/logo-future-eats-invert_2022-08-01/logo-future-eats-invert.png'
+import { ContainerLogin } from "./style";
 
 export const LoginPage = () => {
     return (
-        <div>Login</div>
+        <div>
+            <ContainerLogin>
+                <img width="104px" height="58px" heit src={LogoRappi} alt="logo Rappi" />
+                <strong>Entrar</strong>
+                <input 
+                placeholder="E-mail"
+                />
+                <input
+                placeholder="Senha"
+                />
+                <button>
+                    Entrar
+                </button>
+                <div>
+                    <button>
+                        Não possui cadastro? clique aqui!
+                    </button>
+                </div>
+            </ContainerLogin>
+        </div>
     )
 }
---
+++ src/pages/LoginPage/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ContainerLogin = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    height: 600px;
+    padding: 10px;
+`
