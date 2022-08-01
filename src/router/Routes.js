897341ca552bcbf7df1d5173583c81c13c76330c
Paulo G M Santos <31298118+paulogm15@users.noreply.github.com>
2022-08-01 15:28:27 -0300

    paginas

---
+++ src/router/Routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "../pages/LoginPage/LoginPage";
+import { SignUpPage } from "../pages/SignUpPage/SignUpPage";
+import { SignUpAddress } from "../pages/SignUpAddress/SignUpAddress";
+import { RestaurantPage } from "../pages/Restaurant/Restaurant";
+import { DefaultPage } from "../pages/DefaultPage/DefaultPage";
+import { Feed } from "../pages/Feed/Feed";
+import { CartPage } from "../pages/CartPage/CartPage";
+import { ProfilePage } from "../pages/Profile/Profile";
+import { EditProfile } from "../pages/EditProfile/EditProfile";
+import { EditAddress } from "../pages/EditAddress/EditAddress";
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<LoginPage />} />
+                <Route path="/default" element={<DefaultPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/signup/address" element={<SignUpAddress />} />
+                <Route path="/restaurant" element={<RestaurantPage />} />
+                <Route path="/feed" element={<Feed />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/profile/editprofile" element={<EditProfile />} />
+                <Route path="/profile/editaddress" element={<EditAddress />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
