085f68172598ec2c8c5d2e327745adc723fa284f
Felipe Kaê Martins Prado <tarotalking@gmail.com>
2022-08-02 12:31:05 -0300

    início feed

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,7 +1,50 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 export const Feed = () => {
+
+    const [guardarRestaurantes, setGuardarRestaurantes] = useState([])
+
+    const [searchRestaurant, setSearchRestaurant] = useState("")
+    
+    const headers = {
+        auth: localStorage.getItem("token")
+    };
+
+    const pegarRestaurantes = () => {
+        axios.get("https://us-central1-missao-newton.cloudfunctions.net/rappi4B/restaurants", {headers: headers})
+        .then((response) => {
+            setGuardarRestaurantes(response.data)
+        })
+        .catch((error) => {
+            console.log(error.data)
+        })
+    };
+
+    const handleInputRestaurant = (event) => {
+        setSearchRestaurant(event.target.value)
+    }
+
+    useEffect(() => {
+        pegarRestaurantes()
+    },[]);
+
+    const mapearRestaurantes = guardarRestaurantes.map((post) => {
+        return (
+            <div>
+                {post.name}
+                {post.deliveryTime}
+                {post.shipping}
+            </div>
+        )
+    })
+
     return (
-        <div>Feed</div>
+        <div>
+            <input value={searchRestaurant} onChange={(event) => handleInputRestaurant(event)} placeholder="Buscar Restaurantes"/>
+            Feed
+            {mapearRestaurantes}
+        </div>
     )
 }
